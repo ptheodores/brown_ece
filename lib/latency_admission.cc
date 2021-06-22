b54dc8bbc63e03fdbc22d4eef4c17afdfb74d67c
“ptheodores” <ptheodores5@gmail.com>
2021-06-22 22:12:43 +0000

    small change to printed output

--- lib/latency_admission.cc
+++ lib/latency_admission.cc
@@ -46,5 +46,6 @@ float LatencyAdmission::get_fill_percentage() {
 
 void LatencyAdmission::periodic_output(unsigned long ts, std::ostringstream& outlogfile){
     // Just output the marker and move on
-    outlogfile << " : " << name << " ";
+    //not sure we are doing anything with this right now, but we have it as an option to use?
+    //outlogfile << " : " << name << " ";
 }
