7c00d9505b6d23df71098c15f732319d9ca1c63f
“ptheodores” <ptheodores5@gmail.com>
2021-07-23 15:54:51 +0000

    modified log_mod.py

--- log_mod.py
+++ log_mod.py
@@ -3,29 +3,55 @@
 
 
 import random, sys, errno, os, gzip
+import geoip2.database
 
-def add_args(fout, line):
+
+
+def add_args(fout, line, mode):
 	args_to_add = ""
- 
-	#latency (rand double between 10 and 100)
-	args_to_add += " " + str(random.uniform(10,100)) 
+	if (mode == 1): 
+		#latency (rand double between 10 and 100)
+		args_to_add += " " + str(random.uniform(10,100)) 
 
-	#region (random int, 0 or 1)
-	args_to_add +=  " " + str(random.randint(0,1))
+		#region (random int, 0 or 1)
+		args_to_add +=  " " + str(random.randint(0,1))
+	elif (mode == 2):
+		with geoip2.database.Reader('GeoLite2-City_20210720/GeoLite2-City.mmdb') as reader:
+			try:
+				ip_addr = line.split()[1]
+				response = reader.city(ip_addr)	
+				args_to_add += " " + str(response.location.latitude) + " " + str(response.location.longitude)
+				#print(ip_addr + args_to_add)
+			except ValueError:
+				print(line.split()[1] + "is not a valid IP address")
+				args_to_add += " 0 + 0"
+			except AddressNotFoundError:
+				print("Address " + line.split()[1] + " not found!")
+				args_to_add += " 0 + 0"
 	
-	#write to line
-	args_to_add += '\n'
-	fout.write(line.replace('\n', args_to_add))
+		#write to line
+		args_to_add += '\n'
+		fout.write(line.replace('\n', args_to_add))
 
 def main():
 	print("Running!")
 
-	if len(sys.argv) != 2:
-		print("Usage: log_mod.py <name of directory>")
+	if len(sys.argv) != 3:
+		print("Usage: log_mod.py <name of directory> <mode>")
 		return
 
+	mode = int(sys.argv[2])
+
 	old_dir = str(sys.argv[1])
-	new_dir = "mod_" + old_dir
+	if (mode == 1):
+		name_mod = "mod_"
+	elif (mode == 2):
+		name_mod = "loc_"
+	else:
+		print("Error: Invalid Mode")
+		return -1
+	new_dir = name_mod + old_dir
+
 
 	if not os.path.isdir(old_dir):
 		print(old_dir + " is not a valid directory!")
@@ -44,15 +70,15 @@ def main():
 		#note: to use this for nonzipped log files
 		if filename.endswith('.gz'):
    			with gzip.open(os.path.join(old_dir, filename), 'rt') as fin:
-				with gzip.open(os.path.join(new_dir, "mod_" + filename), "wt") as fout:
+				with gzip.open(os.path.join(new_dir, name_mod + filename), "wt") as fout:
 					for line in fin:
-						add_args(fout, line)
+						add_args(fout, line, mode)
 		else:
 			with open(os.path.join(old_dir, filename), 'rt') as fin:
-				with open(os.path.join(new_dir, "mod_" + filename), "wt") as fout:
+				with open(os.path.join(new_dir, name_mod + filename), "wt") as fout:
 					for line in fin:
-						add_args(fout, line)	
-	print("All done!")
+						add_args(fout, line, mode)	
+	print("Output stored in " + new_dir)
 
 if __name__ == "__main__":
 	main()
